f51a15a00401118c1da541881ef061bbd50799a7
Coco <ck962@outlook.com>
2019-06-27 01:59:54 +0800

    整理汽车品牌数据

--- details.R
+++ details.R
@@ -1,5 +1,7 @@
 library(jsonlite)
 library(tidyverse)
 
-details <- fromJSON('D://Users/ck962/Downloads/taxi_info.json') %>%
-  as.data.frame()
+details <- fromJSON('/home/admin/taxi_info.json') %>%
+  as.data.frame() 
+details[,9] <- gsub("牌.*$|[-0-9A-z/\\.·“”]|(汽车)", "", details[,9])
+colnames(details)
